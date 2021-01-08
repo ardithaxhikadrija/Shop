8b190ca5984155399906e1b592157430c3f83331
ardithaxhikadrija <ardithaxhikadrija1@gmail.com>
2021-01-08 16:21:48 +0100

    Initial commit

---
+++ onlineShopping/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace onlineShopping
+{
+    partial class homeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(homeForm));
+            this.offButton = new System.Windows.Forms.Button();
+            this.minButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // offButton
+            // 
+            this.offButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.offButton.BackColor = System.Drawing.Color.Transparent;
+            this.offButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.offButton.FlatAppearance.BorderSize = 0;
+            this.offButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.offButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.offButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.offButton.Image = ((System.Drawing.Image)(resources.GetObject("offButton.Image")));
+            this.offButton.Location = new System.Drawing.Point(1160, 0);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(40, 38);
+            this.offButton.TabIndex = 0;
+            this.offButton.UseVisualStyleBackColor = false;
+            this.offButton.Click += new System.EventHandler(this.offButton_Click);
+            // 
+            // minButton
+            // 
+            this.minButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minButton.BackColor = System.Drawing.Color.Transparent;
+            this.minButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minButton.FlatAppearance.BorderSize = 0;
+            this.minButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.minButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.minButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minButton.Image = ((System.Drawing.Image)(resources.GetObject("minButton.Image")));
+            this.minButton.Location = new System.Drawing.Point(1114, 0);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(40, 38);
+            this.minButton.TabIndex = 1;
+            this.minButton.UseVisualStyleBackColor = false;
+            this.minButton.Click += new System.EventHandler(this.minButton_Click);
+            // 
+            // homeForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.minButton);
+            this.Controls.Add(this.offButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "homeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button offButton;
+        private System.Windows.Forms.Button minButton;
+    }
+}
+
---
+++ onlineShopping/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace onlineShopping
+{
+    public partial class homeForm : Form
+    {
+        public homeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void offButton_Click(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+    }
+}
