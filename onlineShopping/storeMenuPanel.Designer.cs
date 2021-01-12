b4f9ab298f9c976ff20425095d195602810a1500
ardithaxhikadrija <ardithaxhikadrija1@gmail.com>
2021-01-12 16:12:21 +0100

    storeChanges

---
+++ onlineShopping/storeMenuPanel.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace onlineShopping
+{
+    partial class storeMenuPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(storeMenuPanel));
+            this.aboutusButton = new System.Windows.Forms.Button();
+            this.profilesButton = new System.Windows.Forms.Button();
+            this.ordersButton = new System.Windows.Forms.Button();
+            this.cartButton = new System.Windows.Forms.Button();
+            this.storesButton = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // aboutusButton
+            // 
+            this.aboutusButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.aboutusButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.aboutusButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aboutusButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutusButton.ForeColor = System.Drawing.Color.White;
+            this.aboutusButton.Location = new System.Drawing.Point(0, 380);
+            this.aboutusButton.Name = "aboutusButton";
+            this.aboutusButton.Size = new System.Drawing.Size(197, 75);
+            this.aboutusButton.TabIndex = 11;
+            this.aboutusButton.Text = "About Us";
+            this.aboutusButton.UseVisualStyleBackColor = true;
+            this.aboutusButton.Click += new System.EventHandler(this.aboutusButton_Click);
+            // 
+            // profilesButton
+            // 
+            this.profilesButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.profilesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.profilesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.profilesButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.profilesButton.ForeColor = System.Drawing.Color.White;
+            this.profilesButton.Image = ((System.Drawing.Image)(resources.GetObject("profilesButton.Image")));
+            this.profilesButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.profilesButton.Location = new System.Drawing.Point(0, 304);
+            this.profilesButton.Name = "profilesButton";
+            this.profilesButton.Size = new System.Drawing.Size(197, 75);
+            this.profilesButton.TabIndex = 10;
+            this.profilesButton.Text = "Profile";
+            this.profilesButton.UseVisualStyleBackColor = true;
+            this.profilesButton.Click += new System.EventHandler(this.profilesButton_Click);
+            // 
+            // ordersButton
+            // 
+            this.ordersButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ordersButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.ordersButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ordersButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ordersButton.ForeColor = System.Drawing.Color.White;
+            this.ordersButton.Image = ((System.Drawing.Image)(resources.GetObject("ordersButton.Image")));
+            this.ordersButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ordersButton.Location = new System.Drawing.Point(0, 228);
+            this.ordersButton.Name = "ordersButton";
+            this.ordersButton.Size = new System.Drawing.Size(197, 75);
+            this.ordersButton.TabIndex = 9;
+            this.ordersButton.Text = "Orders";
+            this.ordersButton.UseVisualStyleBackColor = true;
+            this.ordersButton.Click += new System.EventHandler(this.ordersButton_Click);
+            // 
+            // cartButton
+            // 
+            this.cartButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cartButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.cartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cartButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cartButton.ForeColor = System.Drawing.Color.White;
+            this.cartButton.Image = ((System.Drawing.Image)(resources.GetObject("cartButton.Image")));
+            this.cartButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cartButton.Location = new System.Drawing.Point(0, 152);
+            this.cartButton.Name = "cartButton";
+            this.cartButton.Size = new System.Drawing.Size(197, 75);
+            this.cartButton.TabIndex = 8;
+            this.cartButton.Text = "Cart";
+            this.cartButton.UseVisualStyleBackColor = true;
+            this.cartButton.Click += new System.EventHandler(this.cartButton_Click);
+            // 
+            // storesButton
+            // 
+            this.storesButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.storesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.storesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.storesButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.storesButton.ForeColor = System.Drawing.Color.White;
+            this.storesButton.Image = ((System.Drawing.Image)(resources.GetObject("storesButton.Image")));
+            this.storesButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.storesButton.Location = new System.Drawing.Point(0, 76);
+            this.storesButton.Name = "storesButton";
+            this.storesButton.Size = new System.Drawing.Size(197, 75);
+            this.storesButton.TabIndex = 7;
+            this.storesButton.Text = "Stores";
+            this.storesButton.UseVisualStyleBackColor = true;
+            this.storesButton.Click += new System.EventHandler(this.storesButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.homeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.homeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.homeButton.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeButton.ForeColor = System.Drawing.Color.White;
+            this.homeButton.Image = ((System.Drawing.Image)(resources.GetObject("homeButton.Image")));
+            this.homeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.homeButton.Location = new System.Drawing.Point(0, 0);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(197, 75);
+            this.homeButton.TabIndex = 6;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // storeMenuPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.Controls.Add(this.aboutusButton);
+            this.Controls.Add(this.profilesButton);
+            this.Controls.Add(this.ordersButton);
+            this.Controls.Add(this.cartButton);
+            this.Controls.Add(this.storesButton);
+            this.Controls.Add(this.homeButton);
+            this.Name = "storeMenuPanel";
+            this.Size = new System.Drawing.Size(200, 700);
+            this.Load += new System.EventHandler(this.storeMenuPanel_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button aboutusButton;
+        private System.Windows.Forms.Button profilesButton;
+        private System.Windows.Forms.Button ordersButton;
+        private System.Windows.Forms.Button cartButton;
+        private System.Windows.Forms.Button storesButton;
+        private System.Windows.Forms.Button homeButton;
+    }
+}
